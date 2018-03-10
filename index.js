908ddba7b295ae899b0a7d4812cb2d22ec235351
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-10 23:23:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function getRepositories() {
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = repoList
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories)
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
